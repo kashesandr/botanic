f2629bb981ac19c7707343f6d8842fe358ee16ab
kashesandr <alexandresbox@gmail.com>
2023-01-06 09:54:12 +0800

    init

--- app.ts
+++ app.ts
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: process.cwd()+'/.env' });
+
 import app from "./src/index.js";
 
 app.start()
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-
 const start = () => {
     console.log('Hello world');
 };
